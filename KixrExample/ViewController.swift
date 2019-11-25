8c19588bf7918995805c08791ec9366ae062594b
kushwaha03 <kushwahakrishna03@gmail.com>
2019-11-25 16:57:09 +0530

    first new POD file

---
+++ KixrExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KixrExample
+//
+//  Created by Sumit Bangarwa on 11/25/19.
+//  Copyright © 2019 krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
