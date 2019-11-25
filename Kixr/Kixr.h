b271262d4ec260132769dc5fd3bbb58c9a52f834
kushwaha03 <kushwahakrishna03@gmail.com>
2019-11-25 16:45:20 +0530

    Initial Commit

---
+++ Kixr/Kixr.h
@@ -0,0 +1,19 @@
+//
+//  Kixr.h
+//  Kixr
+//
+//  Created by Sumit Bangarwa on 11/25/19.
+//  Copyright © 2019 krishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kixr.
+FOUNDATION_EXPORT double KixrVersionNumber;
+
+//! Project version string for Kixr.
+FOUNDATION_EXPORT const unsigned char KixrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kixr/PublicHeader.h>
+
+
