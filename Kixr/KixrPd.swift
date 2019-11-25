c10135afcef6e58850ea8c8f0657f320b7f6542f
krishna <33350985+kushwaha03@users.noreply.github.com>
2019-11-25 17:19:35 +0530

    add new kixrpd file

---
+++ Kixr/KixrPd.swift
@@ -0,0 +1,20 @@
+//
+//  KixrPd.swift
+//  Kixr
+//
+//  Created by Sumit Bangarwa on 11/25/19.
+//  Copyright © 2019 krishna. All rights reserved.
+//
+
+import Foundation
+public final class KixrPd {
+    
+    let name = "KixrPdLib"
+    
+    public func add(a: String, b: String) -> String {
+        return a + b
+    }
+    
+   
+    
+}
